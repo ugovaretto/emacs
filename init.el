daf17997b063590bc9867eab0ed69fd0edce0169
ugovaretto <ugovaretto@gmail.com>
2024-10-17 13:08:20 +0800

    Moved cheatsheet

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq lexical-binding t)
 (setq inhibit-startup-message t)
 (if (fboundp 'menu-bar-mode) (menu-bar-mode 0) nil)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode 0) nil)
@@ -299,7 +300,14 @@
   :ensure t)
 (setq sly-complete-symbol-function 'sly-flex-completions)
 
-(setq inferior-lisp-program "sbcl")
+;; Allegro CL MacOS
+(setq inferior-lisp-program "/Applications/AllegroCL64express.app/Contents/Resources/alisp")
+;; SBCL
+;;(setq inferior-lisp-program "sbcl")
+
+;; ECL
+;;(setq inferior-lisp-program "ecl")
+
 ;;;;;;;;;; LINES > 80 COLUMNS ;;;;;;;;;;
 (setq-default display-fill-column-indicator-column 79)
 (add-hook 'c++-mode-hook
@@ -364,7 +372,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme yaml-mode wrap-region which-key selectrum-prescient rustic rg rainbow-mode py-autopep8 projectile paredit multiple-cursors modern-cpp-font-lock material-theme mark-multiple magit lsp-ui lsp-treemacs json-mode helm-lsp expand-region elpy ein doom-themes doom-modeline cmake-mode clang-format ccls blacken auto-virtualenv)))
+   '(vterm zenburn-theme yaml-mode wrap-region which-key selectrum-prescient rustic rg rainbow-mode py-autopep8 projectile paredit multiple-cursors modern-cpp-font-lock material-theme mark-multiple magit lsp-ui lsp-treemacs json-mode helm-lsp expand-region elpy ein doom-themes doom-modeline cmake-mode clang-format ccls blacken auto-virtualenv)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
